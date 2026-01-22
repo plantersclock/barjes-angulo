15fcf4ac12ecff2badd207db6688039610e8d537
plantersclock <mattman861@gmail.com>
2026-01-22 16:04:43 +0700

    changed FAQ x margin

--- app/for-clients/faq.tsx
+++ app/for-clients/faq.tsx
@@ -233,7 +233,7 @@ const faqData = [
 const FAQ = () => {
   return (
     <div id="faq" className="bg-linear-to-l from-[#F9F9F9] from-70% to-white">
-      <div className="flex flex-col mx-[10%] sm:mx-[5%] lg:mx-[15%] xl:mx-[23%] 2xl:mx-[20%] ">
+      <div className="flex flex-col  mx-[5%] lg:mx-[15%] xl:mx-[23%] 2xl:mx-[20%] ">
         <h2 className="mt-22 mb-8 text-header text-center">
           Frequently Asked Questions
         </h2>
